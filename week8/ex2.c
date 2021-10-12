15aaf4f01e5f956ac51a1e61974a1a04faff6147
Artemiy3 <71441105+Artemiy3@users.noreply.github.com>
2021-10-12 20:50:31 +0300

    Create ex2.c

---
+++ week8/ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() 
+{
+	char * x;
+	for(int i = 1; i <= 10; i++) {
+		if (x == NULL){
+      return 1;
+    }
+		memset(calloc(2024*1024*1024UL, 1), 0, 2024*1024*1024UL);
+		printf("------- (%d/\%d) Allocated %d MB\n", i, 10,  2024*1024*1024UL/(1024*1024));
+		usleep(1*1000000);
+	}
+	printf("------- Done\n");
+	return 0;
+}
